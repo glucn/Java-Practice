72ad1c4cfec0c0426da5196308ddbadd134ca4ee
YangYuxiao <YangYuxiao@youus-air>
2017-09-10 15:21:16 -0600

    First commit.

---
+++ Hello World/src/BookPile.java
@@ -0,0 +1,33 @@
+/** 
+ *
+ * @author Gary Lu
+ *
+ * A high pile of books is not stable, so it need to be split into some small piles.
+ * 
+ * Input (console): 
+ *   original_pile_height - how many books in the original pile;
+ *   max_hight - the maximum capacity of each pile;
+ *   partition - how many piles should an original pile be spilt into;
+ *   
+ * Expected output (console):
+ *   How many small piles are there in the end.
+ *
+ * Example:
+ *   input: 13, 3, 2 (13 -> 7 + 6 -> 4 + 3 + 3 + 3 -> 2 + 2 + 3 + 3 + 3)
+ *   output: 5
+ *   
+ *   input: 3, 2, 5 (3 -> 1 + 1 + 1)
+ *   output: 3
+ * 
+ */
+
+import java.io.*;
+
+public class BookPile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
